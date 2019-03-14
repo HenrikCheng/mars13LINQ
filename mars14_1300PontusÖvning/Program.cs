4d4f9f96eaa3ed7c6aa8055f9be55a375d1f4633
HenrikCheng <henrikcheng@live.se>
2019-03-14 15:42:57 +0100

    final master

--- mars14_1300PontusÖvning/Program.cs
+++ mars14_1300PontusÖvning/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace mars14_1300PontusÖvning
 {
@@ -17,9 +18,8 @@ namespace mars14_1300PontusÖvning
                 new Person () {Name = "Anna", Age = 79, WorkplaceID = 1},
                 new Person () {Name = "Henrik", Age = 40, WorkplaceID = 1},
             };
-
+            //Håkan och Pontus anekdot
             Random r = new Random();
-
             var slumpad = people
                 .OrderBy(x => r.Next(1, 10000));
 
@@ -29,16 +29,19 @@ namespace mars14_1300PontusÖvning
                 new Workplace () {CompanyName = "Apple", WorkplaceID = 3}
             };
 
+            Console.WriteLine("3a");
             var lista = people
                 .Where(x => x.Age > 30)
-                .OrderBy(x => x.Age)
-                .OrderBy(x => x.Name);
+                //.OrderBy(x => x.Age)
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+            //alternativt sätt att lösa uppgiften
 
             foreach (var item in lista)
             {
                 Console.WriteLine($"name: {item.Name}, age {item.Age}");
             }
-
+            Console.WriteLine("3b");
             var folkUnder30 = people.Count(x => x.Age < 30);
             Console.WriteLine(folkUnder30);
 
@@ -47,36 +50,40 @@ namespace mars14_1300PontusÖvning
 
             var förstaNamnet = people.FirstOrDefault(x => x.Name == "Anna");
             Console.WriteLine(förstaNamnet.Name + ", " + förstaNamnet.Age);
-
-            var joinList = people.Join(company, p => p.WorkplaceID, c => c.WorkplaceID, (p, c) => new { p.Name, c.CompanyName, c.WorkplaceID });
+            //f
+            Console.WriteLine("Uppgift F");
+            var joinList = people
+                .Join(company, p => p.WorkplaceID, c => c.WorkplaceID, (p, c) => new { p.Name, c.CompanyName, c.WorkplaceID });
             foreach (var item in joinList)
             {
                 Console.WriteLine($"Name: {item.Name}, arbetsplats {item.CompanyName}, ref nr: {item.WorkplaceID}");
             }
 
-            var groupJoinList = people.GroupJoin(company, p => p.WorkplaceID, c => c.WorkplaceID, (p, k) => new { PersonName = p.Name, PN = k });
-            foreach (var item in groupJoinList)
-            {
-                Console.Write(item.PersonName);
-                foreach (var innerItem in item.PN)
-                {
-                    Console.WriteLine(innerItem.CompanyName);
-                }
-            }
-            Console.WriteLine();
+            //Console.WriteLine("Uppgift G");
+            //var groupJoinList = people.GroupJoin(company, p => p.WorkplaceID, c => c.WorkplaceID, (p, k) => new { PersonName = p.Name, PN = k });
+            //foreach (var item in groupJoinList)
+            //{
+            //    Console.Write(item.PersonName);
+            //    foreach (var innerItem in item.PN)
+            //    {
+            //        Console.WriteLine(innerItem.CompanyName);
+            //    }
+            //}
+            //Console.WriteLine();
 
+            Console.WriteLine("Uppgift G");
             var groupJoinList2 = company
                 .GroupJoin(people, c => c.WorkplaceID, p => p.WorkplaceID, (c, p) => new { PN = c.CompanyName, Employees = p });
             foreach (var item in groupJoinList2)
             {
-                Console.WriteLine(item.PN);
-                foreach (var innerItem in item.Employees)
-                {
-                    Console.Write($"{innerItem.Name}, ");
-                }
-                Console.WriteLine();
+                Console.WriteLine($"Företagsnamn: {item.PN}, antal anställda: {item.Employees.Count()}");
+                //foreach (var innerItem in item.Employees)
+                //{
+                //    Console.Write($"{innerItem.Name}, ");
+                //}
+                //Console.WriteLine();
             }
-
+            Console.WriteLine("Uppgift E");
             var anställda = groupJoinList2
                 .OrderByDescending(x => x.Employees.Count())
                 .First();
